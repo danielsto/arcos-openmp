fbbf06dcbcc6f167639f09e0a3b74dc955833910
fromage2 <guillermofragio@gmail.com>
2016-11-09 19:32:46 +0100

    Funcion histograma que funciona en Linux con las nuevas matrices

--- Secuencial/ARCfmtut_seq.cpp
+++ Secuencial/ARCfmtut_seq.cpp
@@ -1,123 +1,325 @@
 #include <iostream>
+
 #include <sstream>
+
 #include <fstream>
+
 #include <algorithm>
+
+#include <cmath>
+
 #include <iomanip>
+
+
 
 using namespace std;
 
 int ALTURA;
+
 int ANCHURA;
-int ** matrizR= new int*[ALTURA];
-int ** matrizG= new int*[ALTURA];
-int ** matrizB= new int*[ALTURA];
+
+
+
+string stringRed;
+
+string stringGreen;
+
+string stringBlue;
+
+
+
+
+
+
 
 void dimensiones(){
+
+
+
     string line;
 
-    ifstream myfile ("..\\imagen_entrada"); // Fichero de entrada
+
+
+    ifstream myfile ("imagen_entrada.txt");
+
+
+
     if (myfile.is_open()) // Si existe o lo encuentra
+
     {
+
+
+
         // objeto strin string en el que se almacena la conversion de string a hexadecimal
+
         stringstream hs; //heightstream
+
         stringstream ws; //widthstream
 
 
+
+
+
         getline (myfile,line);
+
         //se eliminan los espacios de la primera linea
+
         line.erase(remove(line.begin(), line.end(), ' '), line.end());
+
         // Obtener a partir de la posición 0 una cadena de longitud 8
+
         hs << hex << line.substr(6,2) + line.substr(4,2) + line.substr(2,2) + line.substr(0,2);
+
         hs >> ALTURA;
 
+
+
         // Obtener a partir de la posición 8 una cadena de longitud 8
+
         ws << hex << line.substr(14,2) + line.substr(12,2) + line.substr(10,2) + line.substr(8,2);
+
         ws >> ANCHURA;
 
+
+
+
+
+
+
         myfile.close();
 
+
+
     }
+
     else cout << "Unable to open file";
+
+
+
 }
 
-void crearImagen(){
-    int decimal;
+
+
+void imagenToString(){
+
+
 
     string line;
-    string imagen;
 
-    unsigned long i;
-    for(i=0; i<ALTURA; i++){
-        matrizR[i]= new int[ANCHURA];
-        matrizG[i]= new int[ANCHURA];
-        matrizB[i]= new int[ANCHURA];
-    }
+    string stringImagen;
 
-    ifstream myfile ("..\\imagen_entrada"); // Fichero de entrada
+
+
+    ifstream myfile ("imagen_entrada.txt"); // Fichero de entrada
+
+
+
     if (myfile.is_open()) // Si existe o lo encuentra
+
     {
+
         while(getline (myfile,line)) {
+
             line.erase(remove(line.begin(), line.end(), '\r'), line.end());
-            imagen += line;
+
+            stringImagen += line;
+
         }
+
         myfile.close();
+
     }
+
     else cout << "Unable to open file";
 
-    imagen.erase(remove(imagen.begin(), imagen.end(), ' '), imagen.end());
-
-    int columna=0;
-    int filas=0;
-
-    for(i=16; i < ALTURA * ANCHURA * 2; i= i + 2){
-        if(columna>ANCHURA){
-            columna=0;
-            filas++;
-        }
-        stringstream ss;
-        ss << hex << imagen.substr(i,2);
-        ss >> decimal;
-        matrizR[filas][columna] = decimal;
-
-        columna++;
-    }
-
-    columna=0;
-    filas=0;
-    for(i; i < ALTURA * ANCHURA * 2 * 2; i= i + 2){
-        if(columna>ANCHURA){
-            columna=0;
-            filas++;
-        }
-        stringstream ss;
-        ss << hex << imagen.substr(i,2);
-        ss >> decimal;
-        matrizG[filas][columna] = decimal;
-        //cout << matrizG[filas][columna];
-        columna++;
 
 
-    }
-
-    columna=0;
-    filas=0;
-    for(i; i < ALTURA * ANCHURA * 2 * 3; i= i + 2){
-        if(columna>ANCHURA){
-            columna=0;
-            filas++;
-        }
-        stringstream ss;
-        ss << hex << imagen.substr(i,2);
-        ss >> decimal;
-        matrizB[filas][columna] = decimal;
-        columna++;
+    stringImagen.erase(remove(stringImagen.begin(), stringImagen.end(), ' '), stringImagen.end());
 
 
-    }
+
+    stringRed= stringImagen.substr(16, ALTURA*ANCHURA*2);
+
+    stringGreen= stringImagen.substr(16 +(ALTURA*ANCHURA*2), ALTURA*ANCHURA*2);
+
+    stringBlue= stringImagen.substr(16 +(ALTURA*ANCHURA*2*2), ALTURA*ANCHURA*2);
+
+
+
 }
 
 
-double **escalaGrises(int **verde, int **rojo, int **azul) {
+
+
+
+int **stringToMatrizR(){
+
+    int **matrizRoja= new int *[ALTURA];
+
+    for(int i=0; i< ALTURA; ++i){
+
+        matrizRoja[i]=new int[ANCHURA];
+
+    }
+
+
+
+    int columna=0;
+
+    int filas=0;
+
+    int decimal=0;
+
+
+
+    int length= stringRed.length();
+
+
+
+    for(int i=0; i <length; i= i + 2){
+
+        if(columna==ANCHURA){
+
+            columna=0;
+
+            filas++;
+
+        }
+
+        stringstream ss;
+
+        ss << hex << stringRed.substr(i,2);
+
+        ss >> decimal;
+
+        matrizRoja[filas][columna] = decimal;
+
+        columna++;
+
+    }
+
+
+
+
+
+    return matrizRoja;
+
+}
+
+
+
+int **stringToMatrizG(){
+
+    int **matrizVerde= new int *[ALTURA];
+
+    for(int i=0; i< ALTURA; ++i){
+
+        matrizVerde[i]=new int[ANCHURA];
+
+    }
+
+
+
+    int columna=0;
+
+    int filas=0;
+
+    int decimal=0;
+
+
+
+    int length= stringGreen.length();
+
+
+
+    for(int i=0; i <length; i= i + 2){
+
+        if(columna==ANCHURA){
+
+            columna=0;
+
+            filas++;
+
+        }
+
+        stringstream ss;
+
+        ss << hex << stringGreen.substr(i,2);
+
+        ss >> decimal;
+
+        matrizVerde[filas][columna] = decimal;
+
+        columna++;
+
+    }
+
+
+
+
+
+    return matrizVerde;
+
+}
+
+
+
+
+
+int **stringToMatrizB(){
+
+    int **matrizAzul= new int *[ALTURA];
+
+    for(int i=0; i< ALTURA; ++i){
+
+        matrizAzul[i]=new int[ANCHURA];
+
+    }
+
+
+
+    int columna=0;
+
+    int filas=0;
+
+    int decimal=0;
+
+
+
+    int length= stringBlue.length();
+
+
+
+    for(int i=0; i <length; i= i + 2){
+
+        if(columna==ANCHURA){
+
+            columna=0;
+
+            filas++;
+
+        }
+
+        stringstream ss;
+
+        ss << hex << stringBlue.substr(i,2);
+
+        ss >> decimal;
+
+        matrizAzul[filas][columna] = decimal;
+
+        columna++;
+
+    }
+
+    return matrizAzul;
+
+}
+
+
+
+
+double **escalaGrises(int **rojo,int **verde, int **azul) {
 
 
     double **grises = new double *[ALTURA];
@@ -158,31 +360,105 @@ void histograma(double **escalagrises, int tramos){
         }
     }
 
+    ofstream outputFile("histogram.txt");
+
     for (int i = 0; i < tramos; i++) {
-        cout << result[i];
-        cout << " ";
+        outputFile << result[i];
+        outputFile << " ";
     }
 
 }
 
-int main() {
+
+
+int main(){
+
+
+
+
+
     dimensiones();
-    crearImagen();
-    resultado = escalaGrises(verde, rojo, azul);
+
+    imagenToString();
+
+    double **resultado= escalaGrises(stringToMatrizR(),stringToMatrizG(),stringToMatrizB());
+    histograma(resultado,25);
 
 
-    /*IMPRESION MATRIZ CAMBIADA*/
-    for (int i = 0; i < ALTURA; i++) {
-        for (int j = 0; j < ANCHURA; j++) {
+    //Impresión de las tres matrices, pruebas
+
+
+
+
+
+    /*for(int i=0; i < ALTURA; ++i){
+
+        for(int j=0; j < ANCHURA; ++j){
+
             cout << resultado[i][j];
+
             cout << " ";
+
         }
-        cout << endl;
+
+        cout << "\n";
+
     }
 
-    cout << endl;
+    cout << "\n";
 
-    histograma(resultado,4);
+    cout << "\n";*/
 
-    return 0;
+
+
+    /*int **resultado2= stringToMatrizG();
+
+
+
+    for(int i=0; i < ALTURA; ++i){
+
+        for(int j=0; j < ANCHURA; ++j){
+
+            cout << resultado2[i][j];
+
+            cout << " ";
+
+        }
+
+        cout << "\n";
+
+    }
+
+
+
+    cout << "\n";
+
+    cout << "\n";
+
+
+
+    int **resultado3= stringToMatrizB();
+
+
+
+    for(int i=0; i < ALTURA; ++i){
+
+        for(int j=0; j < ANCHURA; ++j){
+
+            cout << resultado3[i][j];
+
+            cout << " ";
+
+        }
+
+        cout << "\n";
+
+    }
+
+    cout << "\n";
+
+    cout << "\n";*/
+
+
+
 }
